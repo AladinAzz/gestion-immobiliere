03eafbbd2609e44aa3f44cf5ca52ac3c2f3316b4
AladinAzz <alaaazzouz22@gmail.com>
2024-11-22 23:27:13 +0100

    mise

---
+++ proprietaire/prop.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+
+
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="pic.png" />
+    <link rel="stylesheet" href="l.css">
+    <title>Propriétés à Vendre</title>
+
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.html"><img src="pic.png" alt="aadl 2.0"></a>
+           </div>
+        <ul class="nav-links">
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="list.html">Nos Propriétés</a></li>
+            <li><a href="test3.html">Connexion</a></li>
+        </ul>
+    </nav>
+
+    <footer class="footer">
+        <p>&copy; 2024 AADL 2.0. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
